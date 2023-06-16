cb68c388fef540f7c16b9da97f746928d7849569
hayato94087 <hayato94087@gmail.com>
2023-06-16 18:18:39 +0900

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button, Header } from "ui";
 
 export default function Page() {
+  console.log(process.env.DEBUG_MESSAGE);
   return (
     <>
       <Header text="Web" />
       <Button />
+      <div>{process.env.DEBUG_MESSAGE}</div>
     </>
   );
 }
